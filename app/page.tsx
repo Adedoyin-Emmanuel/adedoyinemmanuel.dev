e4a194fff04137b47e3db7016af2f533d46abba7
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-10-06 02:05:34 +0100

    feat: changed font

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
 const Home = () => {
-  return <h1 className="text-4xl">Hello World, This is my portfilo website</h1>;
+  return (
+    <div>
+      <h1 className="text-4xl">Hello World, This is my portfilo website</h1>
+
+      <p>
+        Research @qside ↗ / @MINDS Lab ↗ (ML + Data). Researcher @ Morgan State
+        Quantum Computing Lab ↗. Prev. AI/ML Engineer Intern @Apple ↗ (Core ML +
+        Infra). Prev. Lead Research @ Tarleton State MISR Lab (Machine Learning
+        Intellligence Research Lab) ↗. Software Engineer - Building ML
+        Architectures + Models, Backend + Infrastructure Systems & Web + Mobile
+        Apps (Light Work).
+      </p>
+    </div>
+  );
 };
 
 export default Home;
