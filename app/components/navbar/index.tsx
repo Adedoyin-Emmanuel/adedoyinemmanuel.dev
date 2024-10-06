fe562ea9e8a137d60df9a1c2a5d89843ec8f0145
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-10-06 02:06:39 +0100

    feat: added navbar component

---
+++ app/components/navbar/index.tsx
@@ -0,0 +1,19 @@
+
+"use client"
+import React from "react";
+
+interface NavbarProps {
+    
+}
+
+const Navbar = ()=> {
+
+    return (
+        <div>
+            <h1>Navbar works!</h1>
+        </div>
+    );  
+}
+
+export default Navbar;
+    
--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,7 @@ const Home = () => {
     <div>
       <h1 className="text-4xl">Hello World, This is my portfilo website</h1>
 
-      <p>
-        Research @qside ↗ / @MINDS Lab ↗ (ML + Data). Researcher @ Morgan State
-        Quantum Computing Lab ↗. Prev. AI/ML Engineer Intern @Apple ↗ (Core ML +
-        Infra). Prev. Lead Research @ Tarleton State MISR Lab (Machine Learning
-        Intellligence Research Lab) ↗. Software Engineer - Building ML
-        Architectures + Models, Backend + Infrastructure Systems & Web + Mobile
-        Apps (Light Work).
-      </p>
+      <p>Hello boy</p>
     </div>
   );
 };
