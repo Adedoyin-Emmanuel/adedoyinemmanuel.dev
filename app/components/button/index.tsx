49706eedc8d5d3e312938857e1b9ad392ac29433
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-10-06 02:07:00 +0100

    feat: added button component

---
+++ app/components/button/index.tsx
@@ -0,0 +1,19 @@
+
+"use client"
+import React from "react";
+
+interface ButtonProps {
+    
+}
+
+const Button = ()=> {
+
+    return (
+        <div>
+            <h1>Button works!</h1>
+        </div>
+    );  
+}
+
+export default Button;
+    
