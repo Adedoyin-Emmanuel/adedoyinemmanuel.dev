1ccbea9d3ec7c984a77b101f017baa792e25b00e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-10-07 14:11:31 +0100

    feat: updated site config

---
+++ config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  domain: "https://adedoyinemmanuel.dev",
+  name: "Adedoyin Emmanuel - Software Engineer",
+  headline: "Adedoyin Emmanuel  | Software Engineer | CS",
+  description: "About Adedoyin Emmanuel Adeniyi - CS, Software Engineer",
+  keywords:
+    "adedoyin, adedoyin emmanuel, software engineer",
+  image: "https://adedoyinemmanuel.dev/emma-3.jpg",
+};
