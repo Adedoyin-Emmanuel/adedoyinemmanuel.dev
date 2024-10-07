872779375fe8c32419607b569a8b691e583c0688
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-10-07 13:55:57 +0100

    feat: added SEO component

---
+++ app/components/Seo/index.tsx
@@ -0,0 +1,19 @@
+
+"use client"
+import React from "react";
+
+interface SeoProps {
+    
+}
+
+const Seo = ()=> {
+
+    return (
+        <div>
+            <h1>Seo works!</h1>
+        </div>
+    );  
+}
+
+export default Seo;
+    
